53209c2f15166ef5b46113809cc902951ebbe817
chirag vijayvergiya <chiragvijayvergiya412@gmail.com>
2024-02-02 03:01:13 +0530

    auth

---
+++ server/Controllers/authController.js
@@ -0,0 +1,57 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const User = require('../models/patientModel');
+const catchAsync = require('../utils/catchAsync');
+
+// ******************************************************************************* //
+
+// This function will generate json web token based on the given id
+const signToken = (id) => {
+  //payload includes the id
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+// ******************************************************************************* //
+
+//This function is responsible for creating a JWT using the signToken function and sending it to the client in the form of a cookie.
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookiesOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
+
+  res.cookie('jwt', token, cookiesOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+// ******************************************************************************* //
+
+// For new user
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+// ******************************************************************************* //
--- server/Routes/patientRoute.js
+++ server/Routes/patientRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const patientController = require('../Controllers/patientConroller');
+const authController = require('../Controllers/authController');
 
 const router = express.Router();
+router.post('/signup', authController.signup);
 
 router
   .route('/')
