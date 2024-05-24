5b054f3ab1bd5e9e41ef2fea4090fa30773b6032
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-05-24 22:11:39 +0530

    Add vercel file in frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+  },
   server: {
     proxy: {
       '/api': {
