28b15023adfe59ccda38c5f27b14c8beec1eb30c
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-05-25 15:14:24 +0530

    sd

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -386,12 +386,13 @@ exports.updatePasswordPatient = catchAsync(async (req, res, next) => {
 });
 
 const logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-    sameSite: 'None',
-    secure: process.env.NODE_ENV === 'production',
-  });
+  // res.cookie('jwt', 'loggedout', {
+  //   expires: new Date(Date.now() + 10 * 1000),
+  //   httpOnly: true,
+  //   sameSite: 'None',
+  //   secure: process.env.NODE_ENV === 'production',
+  // });
+  res.clearCookie('jwt');
   res.status(200).json({ status: 'success' });
 };
 
