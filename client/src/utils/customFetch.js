e984cba3be9eda7ee8bbbccd3f41725d663e7c63
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-05-25 09:52:34 +0530

    M

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -4,5 +4,15 @@ const customFetch = axios.create({
   // baseURL: `http://localhost:8000/api/v1`,
   withCredentials: true,
 });
+customFetch.interceptors.response.use(
+  (response) => {
+    console.log('Response:', response);
+    return response;
+  },
+  (error) => {
+    console.error('Error:', error);
+    return Promise.reject(error);
+  },
+);
 
 export default customFetch;
