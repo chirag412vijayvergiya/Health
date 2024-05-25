01ce97d68550520d51ebdf367d14b0ed8fc3ca39
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-05-25 15:40:37 +0530

    sd

--- client/src/features/authentication/Patients/useUser.js
+++ client/src/features/authentication/Patients/useUser.js
@@ -1,10 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCurrentUser } from '../../../services/apiCommonUser';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 export function useUser() {
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    const userRole = Cookies.get('userRole');
+    if (userRole) {
+      setRole(userRole === 'admin' ? 'doctor' : userRole);
+    }
+  }, []);
+
   const { isPending, data: user } = useQuery({
     queryKey: ['user'],
     queryFn: getCurrentUser,
+    enabled: !!role,
     retry: 0,
   });
 
