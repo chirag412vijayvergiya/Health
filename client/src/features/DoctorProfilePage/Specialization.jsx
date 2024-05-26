35081d57e867509146b7eb445a323cd2a8531791
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-05-26 16:47:09 +0530

    Responsive

--- client/src/features/DoctorProfilePage/Specialization.jsx
+++ client/src/features/DoctorProfilePage/Specialization.jsx
@@ -13,7 +13,7 @@ function Specialization({ doctor }) {
       className="mb-2 flex h-auto w-full flex-col gap-6 rounded-2xl border border-gray-300 bg-gray-50 p-6 tracking-widest shadow-sm shadow-grey-400 dark:border-slate-800 dark:bg-slate-900 dark:shadow-grey-900 md:mb-0 md:h-[28vh] md:w-1/2 md:overflow-y-auto"
     >
       <p
-        className="mx-[2rem] gap-y-3 rounded-xl border-2 border-yellow-500 bg-green-300 px-2 text-sm font-semibold uppercase text-grey-800  peer-disabled:cursor-not-allowed peer-disabled:opacity-70 md:mx-[6.5rem]  md:px-9 "
+        className="mx-auto gap-y-3 rounded-xl border-2 border-yellow-500 bg-green-300 px-2 text-sm font-semibold uppercase text-grey-800 peer-disabled:cursor-not-allowed peer-disabled:opacity-70 md:mx-auto md:px-9"
         htmlFor={doctor.id}
       >
         Specialization
--- client/src/pages/DoctorProfile.jsx
+++ client/src/pages/DoctorProfile.jsx
@@ -12,7 +12,7 @@ function DoctorProfile() {
   if (!doctor) return <div>No doctor found</div>;
 
   return (
-    <div className="my-[2vh] ml-[0.4rem] mr-[0.4rem] flex h-[86vh] flex-col rounded-xl border-r border-r-grey-200 bg-slate-200 p-4 tracking-wider shadow-md shadow-blue-200 dark:border-r-grey-800 dark:bg-gradient-to-r dark:from-slate-800 dark:to-slate-900 dark:shadow-blue-900 md:m-[2vh]">
+    <div className="my-[2vh] ml-[0.4rem] mr-[0.4rem] mt-7 flex h-[86vh] flex-col rounded-xl border-r border-r-grey-200 bg-slate-200 p-4 tracking-wider shadow-md shadow-blue-200 dark:border-r-grey-800 dark:bg-gradient-to-r dark:from-slate-800 dark:to-slate-900 dark:shadow-blue-900 md:m-[2vh]">
       <h1 className="sticky top-0 m-2 w-full bg-slate-200 text-lg font-semibold dark:bg-gradient-to-r dark:from-slate-800 dark:to-slate-900 dark:shadow-blue-900">
         {`${doctor.name}'s Profile`}
       </h1>
